e2660eb276843528e0969f29088c7f237d61e6fb
vanessa <vanesska_denis2011@mail.ru>
2023-02-27 14:57:56 +0300

    ...

---
+++ src/main/java/com/example/storestorage/StoreStorageApplication.java
@@ -0,0 +1,13 @@
+package com.example.storestorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreStorageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/storestorage/StoreStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.storestorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
